320458f90aa3c8cf5b85ec3c49e6d313fc2c09e5
Eddie Shin <eshinhw@gmail.com>
2022-07-03 18:15:03 -0400

    implemented homepage components

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import { Typography } from '@mui/material';
+import { Button } from '@mui/material';
+
+const HomePage = () => {
+  return (
+    <>
+    <Typography variant="h2">Crazy Picasso</Typography>
+
+    <Button variant="contained">Sign Up</Button>
+    <Button variant="contained">Sign In</Button>
+    <Button variant="contained">Create</Button>
+    <Button variant="contained">Join</Button>
+    </>
+  );
+}
+ 
+export default HomePage;
