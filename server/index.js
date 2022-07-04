b20d1aaff18831c8089f6a5bcb4df63e276dc1eb
Jacob <aaron@Jacobs-MacBook-Pro.local>
2022-07-04 18:41:02 -0400

    setup: docker mongo

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 
-var bodyParser = require("body-parser");
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(
+    'mongodb://mongo:27017/crazy-picasso_backend',
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log('MongoDB Connected')
+  })
+  .catch(err => {
+    console.log(err)
+  });
 
 app.use(function(req, res, next) {
   console.log("HTTP request", req.method, req.url, req.body);
@@ -12,6 +26,22 @@ app.use(function(req, res, next) {
 app.get("/", function(req, res, next) {
   res.json("HELLO");
   next();
+});
+
+const Item = require('./src/models/Item');
+
+app.get('/item', (req, res) => {
+  Item.find()
+    .then(items => res.json({items}))
+    .catch(err => res.status(404).json({ msg: 'No items found' }));
+});
+
+app.post('/item/add', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save().then(item => res.json({item}));
 });
 
 const http = require("http");
