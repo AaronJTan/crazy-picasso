8a383bd09db091b73e3b1aed52c0eb1539cbc08c
Eddie Shin <eshinhw@gmail.com>
2022-07-04 22:19:22 -0400

    implemented basic HomePage UI

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,17 +1,38 @@
-import { Typography } from '@mui/material';
-import { Button } from '@mui/material';
+import { Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
+import "../css/HomePage.css";
+import { Fragment } from "react";
 
 const HomePage = () => {
+
+  let navigate = useNavigate();
+
+
+  const goToGamePage = () => {
+    let path = ''
+  }
   return (
     <>
-    <Typography variant="h2">Crazy Picasso</Typography>
-
-    <Button variant="contained">Sign Up</Button>
-    <Button variant="contained">Sign In</Button>
-    <Button variant="contained">Create</Button>
-    <Button variant="contained">Join</Button>
+      <Typography variant="h2" align="center">Crazy Picasso</Typography>
+      <div className="home-btn-container">
+        <Button className="home-btn" variant="contained">
+          Sign Up
+        </Button>
+        <Button className="home-btn" variant="contained">
+          Sign In
+        </Button>
+      </div>
+      <div className="home-btn-container">
+        <Button className="home-btn" id='private-btn' variant="contained">
+          Create Private
+        </Button>
+        <Button className="home-btn" id='public-btn' variant="contained" onClick={() => window.location.href='/game-play'}>
+          Join Public
+        </Button>
+      </div>
     </>
   );
-}
- 
+};
+
 export default HomePage;
