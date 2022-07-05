5d562630acf390088eb3ffc561c0b968ea7a8360
Jacob <aaron@Jacobs-MacBook-Pro.local>
2022-07-05 00:19:07 -0400

    fix: set mongodb connection link via environment variable

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
+/* ------------------------------CONNECT DATABASE------------------------------*/
 const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    'mongodb://mongo:27017/crazy-picasso_backend',
+    process.env.DB_CONNECTION,
     { useNewUrlParser: true }
   )
   .then(() => {
@@ -18,36 +16,26 @@ mongoose
     console.log(err)
   });
 
+/* ------------------------------MIDDLEWARES------------------------------*/
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(function(req, res, next) {
   console.log("HTTP request", req.method, req.url, req.body);
   next();
 });
 
+/* ------------------------------ROUTES------------------------------*/
+/* Temporary*/
 app.get("/", function(req, res, next) {
   res.json("HELLO");
   next();
 });
 
-const Item = require('./src/models/Item');
-
-app.get('/item', (req, res) => {
-  Item.find()
-    .then(items => res.json({items}))
-    .catch(err => res.status(404).json({ msg: 'No items found' }));
-});
-
-app.post('/item/add', (req, res) => {
-  const newItem = new Item({
-    name: req.body.name
-  });
-
-  newItem.save().then(item => res.json({item}));
-});
-
+/* ------------------------------INITIALIZE SERVER------------------------------*/
 const http = require("http");
 const PORT = 5000;
 
-http.createServer(app).listen(PORT, function (err) {
-  if (err) console.log(err);
-  else console.log("HTTP server on http://localhost:%s", PORT);
+http.createServer(app).listen(PORT, () => {
+  console.log(`listening on PORT: ${PORT}`);
 });
