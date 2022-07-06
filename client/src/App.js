80fc21f4fe1c5f98429cba8f90a6a798e3ab6662
Eddie Shin <eshinhw@gmail.com>
2022-07-06 00:48:34 -0400

    changed the route path for GamePage

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import GamePage from './pages/GamePage';
 import HomePage from './pages/HomePage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/game-play" element={<GamePage />} />
+        <Route path="/public/" element={<GamePage />} />
       </Routes>
     </Router>
   );
