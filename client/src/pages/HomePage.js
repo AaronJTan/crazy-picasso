9d4df0898e6c71829368f08bed32cfa56ecbe7bc
Eddie Shin <eshinhw@gmail.com>
2022-07-06 17:18:36 -0400

    created pages for signin and signup and connect them from homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,30 +1,32 @@
 import { Typography } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
 import "../css/HomePage.css";
 import { Fragment } from "react";
 
 const HomePage = () => {
 
-  let navigate = useNavigate();
-
   return (
     <>
       <Typography variant="h2" align="center">Crazy Picasso</Typography>
       <div className="home-btn-container">
-        <Button className="home-btn" variant="contained">
+        <Button className="home-btn" variant="contained" onClick={() => window.location.href="/signup"}>
           Sign Up
         </Button>
-        <Button className="home-btn" variant="contained">
+        <Button className="home-btn" variant="contained" onClick={() => window.location.href="/signin"}>
           Sign In
+        </Button>
+      </div>
+      <div className="home-btn-container">
+        <Button className="home-btn" id='public-btn' variant="contained" onClick={() => window.location.href="/public"}>
+          Join Public
         </Button>
       </div>
       <div className="home-btn-container">
         <Button className="home-btn" id='private-btn' variant="contained">
           Create Private
         </Button>
-        <Button className="home-btn" id='public-btn' variant="contained" onClick={() => window.location.href=`/public`}>
-          Join Public
+        <Button className="home-btn" id='private-btn' variant="contained">
+          Join Private
         </Button>
       </div>
     </>
