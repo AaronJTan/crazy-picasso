7359e24145c54af4d17088ebc2add202c9ad1c01
Jacob <aaron@Jacobs-MacBook-Pro.local>
2022-07-08 23:27:53 -0400

    feat: realtime drawing

--- client/src/components/Canvas/Canvas.jsx
+++ client/src/components/Canvas/Canvas.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 
-function Canvas({socket}) {
+function Canvas({ socket }) {
 
     useEffect(() => {
         handleCanvasActions();
@@ -11,9 +11,12 @@ function Canvas({socket}) {
     const handleCanvasActions = () => {
         const canvas = document.querySelector('#board');
         const ctx = canvas.getContext('2d');
+
+        const clearScreenBtn = document.querySelector('#clear-screen');
+
         const mouseData = { x: 0, y: 0, previousX: 0, previousY: 0 };
         const paintData = { lineWidth: 5, strokeStyle: 'green' };
-        const clearScreenBtn = document.querySelector('#clear-screen');
+        const socketData = {mouseData, paintData};
 
         ctx.fillStyle = "white";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -29,14 +32,14 @@ function Canvas({socket}) {
         canvas.addEventListener('mousemove', handleCanvasMouseXY);
 
         canvas.addEventListener('mousedown', function (e) {
-            canvas.addEventListener('mousemove', draw);
+            canvas.addEventListener('mousemove', handleUserDraw);
         });
 
         canvas.addEventListener('mouseup', function () {
-            canvas.removeEventListener('mousemove', draw);
+            canvas.removeEventListener('mousemove', handleUserDraw);
         });
 
-        const draw = () => {
+        const handleUserDraw = () => {
             ctx.lineJoin = 'round';
             ctx.lineCap = 'round';
             ctx.lineWidth = paintData.lineWidth;
@@ -46,7 +49,23 @@ function Canvas({socket}) {
             ctx.lineTo(mouseData.x, mouseData.y);
             ctx.closePath();
             ctx.stroke();
+
+            socket.emit("drawing", socketData);
         };
+
+        socket.on('live_drawing', handleLiveDrawing);
+
+        function handleLiveDrawing(socketData) {
+            ctx.lineJoin = 'round';
+            ctx.lineCap = 'round';
+            ctx.lineWidth = socketData.paintData.lineWidth;
+            ctx.strokeStyle = socketData.paintData.strokeStyle;
+            ctx.beginPath();
+            ctx.moveTo(socketData.mouseData.previousX, socketData.mouseData.previousY);
+            ctx.lineTo(socketData.mouseData.x, socketData.mouseData.y);
+            ctx.closePath();
+            ctx.stroke();
+        }
 
         const erase = () => {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
