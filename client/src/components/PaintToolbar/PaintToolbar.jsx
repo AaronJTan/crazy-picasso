2f05e12930b0b7172bde74b9b1d7d658f4ffc439
Jacob <aaron@Jacobs-MacBook-Pro.local>
2022-07-09 00:59:22 -0400

    feat: create palette

---
+++ client/src/components/PaintToolbar/PaintToolbar.jsx
@@ -0,0 +1,64 @@
+import { useEffect } from 'react';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+
+function PaintToolBar() {
+
+    useEffect(() => {
+    }, [])
+
+    let colors = [
+        "#FFF",
+        "#C1C1C1",
+        "#EF130B",
+        "#FF7100",
+        "#FFE400",
+        "#00CC00",
+        "#00B2FF",
+        "#231FD3",
+        "#A300BA",
+        "#D37CAA",
+        "#A0522D",
+
+        "#000",
+        "#4C4C4C",
+        "#740B07",
+        "#C23800",
+        "#E8A200",
+        "#005510",
+        "#00569E",
+        "#0E0865",
+        "#550069",
+        "#A75574",
+        "#63300D"
+    ];
+
+    const colorSecondRowIndex = Math.floor(colors.length / 2);
+
+    function Palette() {
+        return (
+            <Box sx={{display: "flex", flexDirection: "column"}}>
+                <Box sx={{display: "flex"}}>
+                    {colors.slice(0, colorSecondRowIndex).map(function (color, i) {
+                        return <Box style={{ width: "24px", height: "24px", border: "1px solid black", backgroundColor: color }}></Box>
+                    })}
+                </Box>
+
+                <Box sx={{display: "flex"}}>
+                    {colors.slice(colorSecondRowIndex, colors.length).map(function (color, i) {
+                        return <Box style={{ width: "24px", height: "24px", border: "1px solid black", backgroundColor: color }}></Box>
+                    })}
+                </Box>
+            </Box>
+        )
+    }
+
+    return (
+        <Container sx={{ display: "flex" }}>
+            <Palette />
+        </Container>
+    );
+};
+
+export default PaintToolBar;
--- client/src/pages/GamePage/GamePage.js
+++ client/src/pages/GamePage/GamePage.js
@@ -4,6 +4,7 @@ import Canvas from "../../components/Canvas/Canvas.jsx"
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import io from "socket.io-client";
+import PaintToolBar from "../../components/PaintToolbar/PaintToolbar";
 
 const GamePage = () => {
   const socket = io.connect(process.env.REACT_APP_SERVER_URL);
@@ -11,12 +12,19 @@ const GamePage = () => {
   return (
     <>
       <Container maxWidth="xl">
-        <Box sx={{ display: 'flex'}}>
-          {/* <Chat /> */}
-          <Canvas socket={socket}/>
-          <Chat socket={socket}/>
+        <Box sx={{ display: 'flex' }}>
+
+          <Box sx={{ display: 'flex', flexDirection: "column", justifyContent: "space-between" }}>
+            <Canvas socket={socket} />
+            <PaintToolBar />
+          </Box>
+
+
+          <Chat socket={socket} />
         </Box>
       </Container>
+
+      <br />
     </>
   );
 }
