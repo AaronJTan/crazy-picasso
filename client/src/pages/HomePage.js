47e677c0df629e3fe444988bea374c841ba4c260
Eddie Shin <eshinhw@gmail.com>
2022-07-09 01:00:46 -0400

    created HomePage.css and removed room buttons

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,20 +1,9 @@
-import { Typography } from "@mui/material";
+import { Link, Typography } from "@mui/material";
 import { Button } from "@mui/material";
 import "../css/HomePage.css";
-import { Fragment } from "react";
 
 const HomePage = () => {
-  const handleGoogleSignin = async () => {
-    await fetch("/auth/google", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-    }).then((res) => {
-      console.log(res);
-    });
-  };
+
 
   const handleSignin = () => {
     window.location.href = "/auth/signin";
@@ -36,28 +25,14 @@ const HomePage = () => {
         <Button className="home-btn" variant="contained" onClick={handleSignin}>
           Sign In
         </Button>
-        <Button className="home-btn" variant="contained" onClick={handleGoogleSignin}>
-          Sign In with Google
-        </Button>
+        {/* using fetch gives CORS error. For now, directly provide URL for Google OAuth */}
+        <Link href="http://localhost:3001/auth/google">
+          <Button className="home-btn" variant="contained">
+            Sign In with Google
+          </Button>
+        </Link>
       </div>
-      <div className="home-btn-container">
-        <Button
-          className="home-btn"
-          id="public-btn"
-          variant="contained"
-          onClick={() => (window.location.href = "/public")}
-        >
-          Join Public
-        </Button>
-      </div>
-      <div className="home-btn-container">
-        <Button className="home-btn" id="private-btn" variant="contained">
-          Create Private
-        </Button>
-        <Button className="home-btn" id="private-btn" variant="contained">
-          Join Private
-        </Button>
-      </div>
+      
     </>
   );
 };
