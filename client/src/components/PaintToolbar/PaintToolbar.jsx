5cbd844feae871ea36e9e421fc2c771361fa8326
Jacob <aaron@Jacobs-MacBook-Pro.local>
2022-07-13 22:03:07 -0400

    add constants folder and file for colors

--- client/src/components/PaintToolbar/PaintToolbar.jsx
+++ client/src/components/PaintToolbar/PaintToolbar.jsx
@@ -2,35 +2,9 @@ import { useEffect } from 'react';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import colors from '../../constants/Colors'
 
 function PaintToolBar({ setPaintData }) {
-
-    let colors = [
-        "#FFF",
-        "#C1C1C1",
-        "#EF130B",
-        "#FF7100",
-        "#FFE400",
-        "#00CC00",
-        "#00B2FF",
-        "#231FD3",
-        "#A300BA",
-        "#D37CAA",
-        "#A0522D",
-
-        "#000",
-        "#4C4C4C",
-        "#740B07",
-        "#C23800",
-        "#E8A200",
-        "#005510",
-        "#00569E",
-        "#0E0865",
-        "#550069",
-        "#A75574",
-        "#63300D"
-    ];
-
     const colorSecondRowIndex = Math.floor(colors.length / 2);
 
     function Palette() {
