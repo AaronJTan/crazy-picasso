0ef158cfb1c9de89f1d81c3387b5f75954128a0c
Eddie Shin <eshinhw@gmail.com>
2022-07-15 01:01:28 -0400

    implemented username and roomCode info in GamePage for dev purpose

--- client/src/pages/GamePage/GamePage.js
+++ client/src/pages/GamePage/GamePage.js
@@ -1,37 +1,45 @@
-import "./GamePage.css"
-import Chat from "../../components/Chat/Chat.jsx"
-import Canvas from "../../components/Canvas/Canvas.jsx"
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import "./GamePage.css";
+import Chat from "../../components/Chat/Chat.jsx";
+import Canvas from "../../components/Canvas/Canvas.jsx";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 import io from "socket.io-client";
 import PaintToolBar from "../../components/PaintToolbar/PaintToolbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const GamePage = () => {
   const socket = io.connect(process.env.REACT_APP_SERVER_URL);
 
-  const [paintData, setPaintData] = useState ({ lineWidth: 5, strokeStyle: 'black' })
+  const [paintData, setPaintData] = useState({ lineWidth: 5, strokeStyle: "black" });
+  const location = useLocation();
+  const username = location.state.username;
+  const roomCode = location.state.roomCode;
+
+  useEffect(() => {
+    socket.emit("join_room", roomCode);
+  });
 
   return (
     <>
       {paintData.strokeStyle}
-
+      <h1>
+        My username is {username}. I join the room code: {roomCode}
+      </h1>
       <Container maxWidth="xl">
-        <Box sx={{ display: 'flex' }}>
-
-          <Box sx={{ display: 'flex', flexDirection: "column", justifyContent: "space-between" }}>
-            <Canvas socket={socket} paintData={paintData}/>
+        <Box sx={{ display: "flex" }}>
+          <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
+            <Canvas socket={socket} paintData={paintData} />
             <PaintToolBar setPaintData={setPaintData} />
           </Box>
 
-
-          <Chat socket={socket} />
+          <Chat username={username} roomCode={roomCode} socket={socket} />
         </Box>
       </Container>
 
       <br />
     </>
   );
-}
+};
 
 export default GamePage;
