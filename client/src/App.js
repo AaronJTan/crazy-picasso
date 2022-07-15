5d21eec2c542f0134160d980136464fd8848c493
Eddie Shin <eshinhw@gmail.com>
2022-07-15 00:55:25 -0400

    relocated react routes from App to index

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,3 @@
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import GamePage from "./pages/GamePage";
-// import HomePage from "./pages/HomePage";
-// import SignUp from "./pages/SignupPage";
-// import SignIn from "./pages/SigninPage";
-// import "./App.css";
-// import SelectRoomPage from "./pages/SelectRoomPage";
-// import React, {Component} from "react";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<HomePage />} />
-//         <Route path="/select-room" element={<SelectRoomPage />} />
-//         <Route path="/public" element={<GamePage />} />
-//         <Route path="/private" element={<GamePage />} />
-//         <Route path="/auth/signup/" element={<SignUp />} />
-//         <Route path="/auth/signin/" element={<SignIn />} />
-//       </Routes>
-//     </Router>
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import GamePage from './pages/GamePage/GamePage';
-import HomePage from './pages/HomePage/HomePage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import GamePage from "./pages/GamePage/GamePage";
+import HomePage from "./pages/HomePage/HomePage";
+import SignUp from "./pages/SignupPage";
+import SignIn from "./pages/SigninPage";
+import SelectRoomPage from "./pages/SelectRoomPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -14,10 +17,11 @@ root.render(
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
           <Route path="/game-play" element={<GamePage />} />
+          <Route path="/auth/signup/" element={<SignUp />} />
+          <Route path="/auth/signin/" element={<SignIn />} />
+          <Route path="/select-room" element={<SelectRoomPage />} />
         </Route>
-
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
-
