1bf7f0d5d3d217161eadcd792eca1592a6be46b1
Eddie Shin <eshinhw@gmail.com>
2022-07-17 00:50:04 -0400

    implemented local signup and signin routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,19 +1,20 @@
 const router = require("express").Router();
-const passport = require("passport");
-const { signupPlayer } = require("../controllers/playerController");
+// const passport = require("passport");
+const { signupPlayer, signinPlayer } = require("../controllers/playerController");
 
 // defines endpoints for signup and signin
 router.post("/signup", signupPlayer);
+router.post("/signin", signinPlayer);
 
-router.get("/google", passport.authenticate("google", { scope: ["email", "profile"] }));
+// router.get("/google", passport.authenticate("google", { scope: ["email", "profile"] }));
 
 // callback route for google to redirect to
-router.get(
-  "/google/redirect",
-  passport.authenticate("google"),
-  (req, res) => {
-    res.send("you reached the callback URI");
-  }
-);
+// router.get(
+//   "/google/redirect",
+//   passport.authenticate("google"),
+//   (req, res) => {
+//     res.send("you reached the callback URI");
+//   }
+// );
 
 module.exports = router;
