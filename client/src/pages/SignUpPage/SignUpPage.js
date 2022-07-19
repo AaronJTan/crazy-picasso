fef9ab1be3c281ca6e86326ee78c1245a8a27a0f
Eddie Shin <eshinhw@gmail.com>
2022-07-18 23:53:54 -0400

    enabled buttons on signup/signin pages

--- client/src/pages/SignUpPage/SignUpPage.js
+++ client/src/pages/SignUpPage/SignUpPage.js
@@ -28,6 +28,12 @@ export default function SignUp() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (!firstName || !lastName || !username || !email || !password) {
+      alert("Missing Inputs");
+      return;
+    }
+
     const player = { firstName, lastName, username, email, password };
 
     console.log("hit handleSubmit signup");
@@ -56,22 +62,88 @@ export default function SignUp() {
     <>
       <h1 className="animate__animated animate__rotateIn">Sign Up</h1>
       <form className="sign-up-form">
-        <label className="animate__animated animate__lightSpeedInLeft animate__delay-1s" for="first-name">First Name</label>
-        <input className="animate__animated animate__lightSpeedInRight animate__delay-1s" type="text" id="first-name" />
+        <label
+          className="animate__animated animate__lightSpeedInLeft animate__delay-1s"
+          for="first-name"
+        >
+          First Name
+        </label>
+        <input
+          className="animate__animated animate__lightSpeedInRight animate__delay-1s"
+          type="text"
+          id="first-name"
+          onChange={e => setFirstName(e.target.value)}
+          value={firstName}
+          required
+        />
 
-        <label className="animate__animated animate__lightSpeedInLeft animate__delay-1s" for="last-name">Last Name</label>
-        <input className="animate__animated animate__lightSpeedInRight animate__delay-1s" type="text" id="last-name" />
+        <label
+          className="animate__animated animate__lightSpeedInLeft animate__delay-1s"
+          for="last-name"
+          
+        >
+          Last Name
+        </label>
+        <input
+          className="animate__animated animate__lightSpeedInRight animate__delay-1s"
+          type="text"
+          id="last-name"
+          onChange={e => setLastName(e.target.value)}
+          value={lastName}
+          required
+        />
 
-        <label className="animate__animated animate__lightSpeedInLeft animate__delay-1s" for="username">Username</label>
-        <input className="animate__animated animate__lightSpeedInRight animate__delay-1s" type="text" id="username" />
+        <label
+          className="animate__animated animate__lightSpeedInLeft animate__delay-1s"
+          for="username"
+        >
+          Username
+        </label>
+        <input
+          className="animate__animated animate__lightSpeedInRight animate__delay-1s"
+          type="text"
+          id="username"
+          onChange={e => setUsername(e.target.value)}
+          value={username}
+          required
+        />
 
-        <label className="animate__animated animate__lightSpeedInLeft animate__delay-1s" for="email">Email</label>
-        <input className="animate__animated animate__lightSpeedInRight animate__delay-1s" type="text" id="email" />
+        <label
+          className="animate__animated animate__lightSpeedInLeft animate__delay-1s"
+          for="email"
+        >
+          Email
+        </label>
+        <input
+          className="animate__animated animate__lightSpeedInRight animate__delay-1s"
+          type="text"
+          id="email"
+          onChange={e => setEmail(e.target.value)}
+          value={email}
+          required
+        />
 
-        <label className="animate__animated animate__lightSpeedInLeft animate__delay-1s" for="password">Password</label>
-        <input className="animate__animated animate__lightSpeedInRight animate__delay-1s" type="password" id="password" />
+        <label
+          className="animate__animated animate__lightSpeedInLeft animate__delay-1s"
+          for="password"
+        >
+          Password
+        </label>
+        <input
+          className="animate__animated animate__lightSpeedInRight animate__delay-1s"
+          type="password"
+          id="password"
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+          required
+        />
 
-        <button className="button animate__animated animate__zoomIn animate__delay-2s">Sign Up with Username</button>
+        <button
+          className="button animate__animated animate__zoomIn animate__delay-2s"
+          onClick={handleSubmit}
+        >
+          Sign Up with Username
+        </button>
       </form>
     </>
   );
