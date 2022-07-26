ef7f46ccc5c9d90cadaaeec90ac586426ffe3870
Jacob <aaron@Jacobs-MacBook-Pro.local>
2022-07-25 23:32:44 -0400

    fix: create functions to add user to users list and to delete from users list

--- server/socketHandler.js
+++ server/socketHandler.js
@@ -1,9 +1,26 @@
 const users = [];
 
+const addUserToUsersList = (io, username) => {
+  // looping over the io.of("/").sockets object,
+  // a map of all currently connected socket instances indexed by ID
+  for (let [id, socket] of io.of("/").sockets) {
+    if (!users.includes(username)) {
+      users.push(username);
+    }
+  }
+}
+
+const deleteUserFromList = (username) => {
+  let index = users.findIndex((user) => {
+    return user === username;
+  })
+
+  users.splice(index, 1);
+}
+
 const listen = (io) => {
   // Socket middleware to check the username and allow the connection
   io.use((socket, next) => {
-    console.log("io middleware");
     const username = socket.handshake.auth.username;
     console.log("io middleware username: ", username);
     // username is added as an attribute of socket object which can be reused later
@@ -12,14 +29,8 @@ const listen = (io) => {
   });
 
   io.on("connection", (socket) => {
-    // looping over the io.of("/").sockets object,
-    // a map of all currently connected socket instances indexed by ID
-    for (let [id, socket] of io.of("/").sockets) {
-      if (!users.includes(socket.username)) {
-        users.push(socket.username);
-      }
-    }
-    // send user list to connected clients => gives an error / slowdown
+    addUserToUsersList(io, socket.username);
+
     socket.broadcast.emit("user_joined", users);
     socket.to("public").emit("receive_message", { roomCode: "public", author: socket.username, message: "JOINED THE GAME" });
 
@@ -39,11 +50,7 @@ const listen = (io) => {
     });
 
     socket.on("disconnect", () => {
-      let index = users.findIndex((user) => {
-        return user == socket.username;
-      })
-
-      users.splice(index, 1);
+      deleteUserFromList(socket.username);
 
       socket.broadcast.emit("user_disconnected", users);
       socket.to("public").emit("receive_message", { roomCode: "public", author: socket.username, message: "LEFT THE GAME" });
