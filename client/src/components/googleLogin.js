b792c7c5f9218f294a60c9a5e2ed16a20403e4d9
Eddie Shin <eshinhw@gmail.com>
2022-07-28 16:35:30 -0400

    enabled google oauth login and logout buttons

---
+++ client/src/components/googleLogin.js
@@ -0,0 +1,31 @@
+import { GoogleLogin } from "react-google-login";
+import { useNavigate } from "react-router-dom";
+import AuthService from "../services/AuthService";
+
+const CLIENT_ID = "219662561135-pm8u2seq0jn6h9bebu6j86jlrjv8pmsi.apps.googleusercontent.com";
+
+export default function GoogleLoginButton() {
+  const navigate = useNavigate();
+  const onSuccess = async (res) => {
+    console.log("LOGIN SUCCESS! Current user: ", res.profileObj);
+    const loginPayload = { username: res.profileObj.name, googleId: res.profileObj.googleId };
+    AuthService.login(loginPayload).then(response => {
+      navigate("/", { state: {username: res.profileObj.name}});
+    });
+  };
+  const onFailure = (res) => {
+    console.log("LOGIN Failed! res: ", res);
+  };
+  return (
+    <div id="googleSignInButton">
+      <GoogleLogin
+        clientId={CLIENT_ID}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={"single_host_origin"}
+        isSignedIn={true}
+      />
+    </div>
+  );
+}
