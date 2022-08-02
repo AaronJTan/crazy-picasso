a8365cd7baf3797b5adebb5d3af8c6c65e707829
Eddie Shin <eshinhw@gmail.com>
2022-08-01 22:18:54 -0400

    implemented frontend react timer

--- client/src/components/Timer/DrawingTimer.js
+++ client/src/components/Timer/DrawingTimer.js
@@ -6,12 +6,10 @@ export default function DrawingTimer({ setWord, roomCode, socketRef, expiryTimes
     seconds,
     minutes,
     isRunning,
-    start,
-    pause,
-    resume,
-    restart,
   } = useTimer({ expiryTimestamp, onExpire: () => {
     setWord("");
+    console.log("onExpire");
+
     socketRef.current.emit("timer_is_up", {roomCode}, () => {
       console.log(roomCode);
       console.log("timer_is_up emitted");
@@ -28,18 +26,6 @@ export default function DrawingTimer({ setWord, roomCode, socketRef, expiryTimes
       <div style={{fontSize: '50px'}}>
         <span>{twoDigitFormat(minutes)}</span>:<span>{twoDigitFormat(seconds)}</span>
       </div>
-      <div style={{fontSize: '20px'}}>
-        <p>{isRunning && `${currentDrawerUsername} is drawing...`}</p>
-      </div>
-      {/* <button onClick={start}>Start</button>
-      <button onClick={pause}>Pause</button>
-      <button onClick={resume}>Resume</button> */}
-      {/* <button onClick={() => {
-        // Restarts to 5 minutes timer
-        const time = new Date();
-        time.setSeconds(time.getSeconds() + 300);
-        restart(time)
-      }}>Restart</button> */}
     </div>
   );
 }
